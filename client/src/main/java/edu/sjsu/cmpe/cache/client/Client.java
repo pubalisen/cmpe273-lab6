6f9cf62b437f6f2b507da884f21457cba31b04dc
pubalisen <pubali.sen@sjsu.edu>
2014-04-30 14:19:22 -0700

    Implementing Consistent Hashing

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,17 +1,62 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import com.google.common.hash.Hashing;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+              
+        HashMap<DistributedCacheService, Integer> ConsMap = new HashMap<DistributedCacheService, Integer>();
+        
+        ConsMap.put(new DistributedCacheService("http://localhost:3000"), 0);
+        ConsMap.put(new DistributedCacheService("http://localhost:3001"), 1);
+        ConsMap.put(new DistributedCacheService("http://localhost:3002"), 2);
+       
+              
+        int size = ConsMap.size();
+        
+        //System.out.println(size);
+        
+        for (int i=1; i<=10; i++){
+        	
+        	int bucket = Hashing.consistentHash( Hashing.md5().hashInt(i), size);
+           // System.out.println(bucket);
+            
+            for (Integer value : ConsMap.values()) {
+            	if ( bucket == value) {
+            	String[] str ={"x", "a", "b", "c", "d", "e","f", "g", "h", "i", "j",};
+            	//System.out.println(strChars[i]);
+            		
+            		          	//System.out.println("Value = " + value);
+            	
+            	
+            	Iterator<Map.Entry<DistributedCacheService, Integer>> entries = ConsMap.entrySet().iterator();
+                while (entries.hasNext()) {
+                	
+                	
+                    Map.Entry<DistributedCacheService, Integer> entry = entries.next();
+                    if ( bucket == entry.getValue()) {
+                   // System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+                    
+                    entry.getKey().put(i, str[i]);
+                    System.out.println("put(" + i + ") => " + str[i]);
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+                    String values = entry.getKey().get(i);
+                    System.out.println("get(" + i + ") => " + values);
+                    
+                    }
+                }
+                
+            	}
+            }       	
+            }
+          
+        
 
         System.out.println("Existing Cache Client...");
     }
